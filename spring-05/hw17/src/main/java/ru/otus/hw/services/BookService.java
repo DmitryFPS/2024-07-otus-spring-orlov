81701904e7ae333331e77ca690b64db891775a95
Dmitriy <ddd12@mail.ru>
2024-12-08 17:16:09 +0300

    hw17

---
+++ spring-05/hw17/src/main/java/ru/otus/hw/services/BookService.java
@@ -0,0 +1,21 @@
+package ru.otus.hw.services;
+
+import ru.otus.hw.dto.BookCreateDto;
+import ru.otus.hw.dto.BookDto;
+import ru.otus.hw.dto.BookUpdateDto;
+
+import java.util.List;
+
+public interface BookService {
+    BookDto findById(final Long id);
+
+    BookUpdateDto getBookUpdateDtoById(final Long id);
+
+    List<BookDto> findAll();
+
+    BookDto create(final BookCreateDto book);
+
+    BookDto update(final BookUpdateDto book);
+
+    void deleteById(final Long id);
+}
